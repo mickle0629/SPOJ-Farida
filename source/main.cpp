ce47c2f8823c6a147dd2b1d6c1fea064b9f5d483
mickle0629 <mikeyd2k2@gmail.com>
2023-11-03 20:19:03 -0700

    farida complete, possib. input issue

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,14 @@
 #include <vector>
 #include <map>
 int maxCoins(std::vector<int> monsters) {
+    if (monsters.size() == 2) {
+        return std::min(monsters[0], monsters[1]);
+    }
+    else if (monsters.size() == 1) {
+        return monsters[0];
+    } else if (monsters.empty()) {
+        return 0;
+    }
     std::map<int, int> dp;
     dp[0] = monsters[0];
     dp[1] = monsters[1];
@@ -15,8 +23,33 @@ int maxCoins(std::vector<int> monsters) {
 }
 
 int main() {
-    printf("Hello World");
-    std::vector<int> monsters = {1,2,3,4,5};
-    std::cout << maxCoins(monsters) << std::endl;
+    bool testing = false;
+    if (testing) {
+        std::vector<int> monsters = {};
+        std::cout << maxCoins(monsters) << std::endl;
+    }
+    
+    else {
+        int noTestCases = 0;
+        std::cin >> noTestCases;
+        std::vector<int> results = {};
+
+        for (int i = 0; i < noTestCases; i++) {
+            int arrayLength;
+            std::cin >> arrayLength;
+            std::vector<int> inputArray = {};
+
+            for (int i = 0; i < arrayLength; i++) {
+                int input;
+                std::cin >> input;
+                inputArray.push_back(input);
+            }
+
+            results.push_back(maxCoins(inputArray));
+        }
+        for (int result : results) {
+            std::cout << result << " " << std::endl;
+        }
+    }
     return 0;
 }

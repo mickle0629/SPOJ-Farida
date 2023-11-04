341e309a80591cc8253965e688cf06bb68e90efe
mickle0629 <mikeyd2k2@gmail.com>
2023-11-03 19:18:19 -0700

    algo done. need stdin

--- source/main.cpp
+++ source/main.cpp
@@ -4,21 +4,19 @@
 #include <map>
 int maxCoins(std::vector<int> monsters) {
     std::map<int, int> dp;
-    dp[monsters[0]] = monsters[0];
-    dp[monsters[1]] = monsters[1];
-    // for every monster starting from the 3rd one
-    for (int i = 2; i < monsters.size(); i++) {
-        dp[monsters[i]] = std::max((monsters[i] + dp[monsters[i - 2]]), dp[monsters[i - 1]]);
+    dp[0] = monsters[0];
+    dp[1] = monsters[1];
+    
+    for (int i = 2; i < monsters.size(); i++) { // for every monster starting from the 3rd one
+        dp[i] = std::max((monsters[i] + dp[i - 2]), dp[i - 1]); // Compute the dp value for that monster
     }
-        // Compute the dp value for that monster
-        
         // return value
-        return 0;
+    return dp[dp.size() - 1];
 }
 
 int main() {
     printf("Hello World");
-    std::vector<int> monsters = {25,1,45,42,1,8,9};
-    maxCoins(monsters);
+    std::vector<int> monsters = {1,2,3,4,5};
+    std::cout << maxCoins(monsters) << std::endl;
     return 0;
 }
